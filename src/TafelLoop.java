87c13d043908fdd9cecf685f142ccd231b84957a
JWahyu <71404267+JWahyu@users.noreply.github.com>
2020-09-30 04:30:26 +0200

    Jade / JWahyu
    Opdracht TafelLoop

--- src/TafelLoop.java
+++ src/TafelLoop.java
@@ -1,2 +1,43 @@
+import java.util.Scanner;
+
 public class TafelLoop {
+    public static void main(String[] args) {
+
+        // Tafel met invoer.
+
+        System.out.println("Wat is je lievelingsgetal?");
+
+        var input = new Scanner(System.in);
+
+        var input1 = input.nextInt();
+
+        System.out.println();
+        System.out.println("Ik zal het voor je in een tafel omrekenen!");
+
+        for(var multiplicationNumber = 1; multiplicationNumber <=10; multiplicationNumber++){
+            System.out.println(multiplicationNumber + " x " + input1 + " = " + input1 * multiplicationNumber);
+        }
+
+        System.out.println();
+        System.out.println("―――――――――――――――――――――");
+        System.out.println();
+
+        // Tweede tafel met invoer en alternatief code.
+
+        System.out.println("Welk getal vind je niet leuk?");
+
+        var input2 = input.nextInt();
+
+        System.out.println();
+        System.out.println("Dit zal geen leuke tafel voor je worden!");
+
+        for(var year = 1; year <= 10; year++){
+            System.out.format("%d %s %d %s %d %n", year, " x ", input2, " = ", input2*year);
+        }
+
+        System.out.println();
+
+        // Humor.
+        System.out.println("Bah!");
+    }
 }
